7497bfcec1d92fd73aa9f673601a1dba97c68b75
Mafrans <malte.kluft@gmail.com>
2023-08-04 22:01:18 +0200

    fix: keep it simple stupid

--- install.sh
+++ install.sh
@@ -1,15 +1,2 @@
-INSTALL_DIR=/tmp/d`echo $RANDOM`
-git clone https://github.com/Mafrans/arch-configs.git $INSTALL_DIR
-
-DEPENDENCIES=$(cat $INSTALL_DIR/dependencies.txt | xargs)
-if command -v paru
-then
-  paru -Sy $DEPENDENCIES
-elif command -v yay
-then
-  yay -Sy $DEPENDENCIES
-fi
-
-find $INSTALL_DIR -maxdepth 1 -not -iname ".*" -execdir cp -r {} ~/.config \;
-rm -rf $INSTALL_DIR
+find . -maxdepth 1 -not -iname ".*" -execdir cp -r {} ~/.config \;
 
