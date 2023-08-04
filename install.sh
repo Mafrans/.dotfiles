9842db11382f69d2fd2ce467ff893c33ed62e7bf
Mafrans <malte.kluft@gmail.com>
2023-08-04 21:57:09 +0200

    fix: update install file

--- install.sh
+++ install.sh
@@ -1,12 +1,15 @@
-INSTALL_DIR=/tmp/d`$RANDOM|md5sum`
+INSTALL_DIR=/tmp/d`echo $RANDOM`
+git clone https://github.com/Mafrans/arch-configs.git $INSTALL_DIR
 
-if [ which paru ] then
-  echo $INSTALL_DIR/dependencies.txt | paru -Sy
-elif [ which yay ] then
-  echo $INSTALL_DIR/dependencies.txt | yay -Sy
+DEPENDENCIES=$(cat $INSTALL_DIR/dependencies.txt | xargs)
+if command -v paru
+then
+  paru -Sy $DEPENDENCIES
+elif command -v yay
+then
+  yay -Sy $DEPENDENCIES
 fi
 
-rm -rf $INSTALL_DIR
-git clone https://github.com/Mafrans/arch-configs.git $INSTALL_DIR
 find $INSTALL_DIR -maxdepth 1 -not -iname ".*" -execdir cp -r {} ~/.config \;
+rm -rf $INSTALL_DIR
 
